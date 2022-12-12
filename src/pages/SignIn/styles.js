3f7cb09644339e36fb55dcdd026fd121e5112c7a
Raul Francisco Effting <rauleffting94@gmail.com>
2022-12-12 19:35:28 -0300

    fixed layout signIn page

--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -2,8 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 100%;
-  height: 100%;
-  padding-bottom: 100px;
+  height: 100vh;
 
   display: flex;
   align-items: center;
